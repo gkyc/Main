85c3ed12a5a3009e664008085484688f9e56f3f5
Pananann <44856768+Pananann@users.noreply.github.com>
2019-03-07 13:24:24 +0800

    Update InforServlet.java

--- src/com/yc/servlet/InforServlet.java
+++ src/com/yc/servlet/InforServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.yc.util.JDBCConnect;
+import com.yc.utils.JDBCConnect;
 
 @WebServlet("/infor")
 public class InforServlet extends HttpServlet {
